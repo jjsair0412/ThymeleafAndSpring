d2defa4de997056498e56a6bfc2ef4f3cd4de380
주진성 <jjsair0412@naver.com>
2021-08-15 01:28:40 +0900

    Initial commit

---
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -0,0 +1,75 @@
+package hello.itemservice.web.form;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/form/items")
+@RequiredArgsConstructor
+public class FormItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "form/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "form/item";
+    }
+
+    /**
+     * 타임리프를 사용하고 싶다면, 아무런 값이 없는 Item이라도 model로 넘겨줘야한다.
+     * model.addAttribute("item",new Item());
+     * 여기서 Item이란 dto나 entity같이 데이터를 전달하는 역할을 한다.
+     * 상품명과 가격, 상품번호등이 저장되는 프로퍼티이다.
+     *
+     * 빈객체 하나를 넘겨준다고해서 큰 변화가 일어나거나 비용이 많이 일어나지 않는다.
+     * 그니까 괜찮당
+     */
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        model.addAttribute("item",new Item());
+        return "form/addForm";
+    }
+
+    @PostMapping("/add")
+    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/form/items/{itemId}";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    /**
+     * edit에서는 이미 item을 넘겨주고 있다.
+     * 타임리프를 사용하기 위해 빈 item을 넘길 필요가 없다.
+     */
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "form/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/form/items/{itemId}";
+    }
+
+}
+
